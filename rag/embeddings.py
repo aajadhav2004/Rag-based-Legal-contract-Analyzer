a736ef217c16a6ec8811e00acc0740bb06b21c9b
Avinash Jadhav <avinashjadhav2468@gmail.com>
2026-03-13 19:32:33 +0530

    reduce embedding model size to MiniLM L6

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -3,7 +3,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 def load_embeddings():
 
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
     return embeddings
